be86f5fce10b2264a54c00b7a542900f2cf826e2
Remy <remy.tupenot@gmail.com>
2020-06-30 16:06:25 +0200

    Chat with rooms

--- server.js
+++ server.js
@@ -8,8 +8,18 @@ var connectfour = {rooms : { } } ;
 
 io.on('connection', function (socket) {
     console.log("A user connected");
-    socket.on('message', function(message){
-      socket.broadcast.emit('message',{message:message});
+
+
+    socket.on('message', function(data){
+      socket.to(data['room']).broadcast.emit('message',data['message']);
+    });
+
+    socket.on('username-change', function(username){
+      socket.username = username;
+    });
+
+    socket.on('join-room', function(roomId){
+      socket.join(roomId);
     });
 });
 
