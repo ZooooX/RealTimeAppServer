3da4bda1afe456ed4c6b989a2ac8baf8a9615f6c
Remy <remy.tupenot@gmail.com>
2020-07-11 18:39:21 +0200

    Fix probleme de duplication d'event

--- server.js
+++ server.js
@@ -16,31 +16,36 @@ io.on('connection', function (socket) {
     console.log("A user connected");
 
 
-
+    //si l'utilisateur change son profile
     socket.on('profile-change', function(data){
       socket.username = data['username'];
       socket.color = data['color'];
     });
 
+    //join room + listen to messages
     socket.on('join-room', function(roomId){
       socket.removeAllListeners('message');
+
       socket.join(roomId);
-      console.log(socket._events);
 
       console.log('user '+ socket.username + ' joined room ' + roomId);
+
+      socket.to(roomId).emit('room-joined',{username: socket.username});
 
       socket.on('message', function(data){
         socket.in(data['room']).emit('message',{message : data['message'],username : socket.username, color : socket.color});
       });
     });
 
+    //leave room
     socket.on('leave-room', function(roomId){
       socket.leave(roomId, function (err) {
         console.log('user '+ socket.username + ' left room ' + roomId);
-        console.log(socket._events);
+        socket.to(roomId).emit('room-left',{username: socket.username});
       });
     });
 
+    //renvoi les salles de jeux actuelles
     socket.on('load-rooms', function(game){
       if (game === GAMES[1]) {
         socket.emit('rooms',tictactoe.rooms);
@@ -50,6 +55,7 @@ io.on('connection', function (socket) {
       }
     });
 
+    //création d'une nouvelle salle pour le bon jeu
     socket.on('new-room', function(data){
       if (data.game === GAMES[1]) {
         let newRoom = {roomId : data.roomId};
